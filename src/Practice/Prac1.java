379d7a14e6f7228db9210e9334cc81e72472b8f4
Jyothi <159113395+JyothiRaavi@users.noreply.github.com>
2025-04-04 13:52:31 +0530

    Update Prac1.java

--- src/Practice/Prac1.java
+++ src/Practice/Prac1.java
@@ -4,6 +4,15 @@ public class Prac1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Execution started....");
+		try{
+		System.out.println("24/0");
+			}
+		catch(ArithmeticException e) {
+			System.out.println("There is an exception once check");
+		}
+		System.out.println("Program terminated successfully.....:)");
+		
 
 	}
 
